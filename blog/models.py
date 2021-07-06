6706fc6143d8c63268a630b97267797c7dfd9651
Raghu <raghuveer.chin@gmail.com>
2021-07-06 08:27:09 +0530

    Done with the protfolio project

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,16 @@ class Blog(models.Model):
     pub_data = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        if len(self.body) < 100:
+            return self.body
+        else:
+            return self.body[:100] + " ..."
+
+    def pub_data_fmt(self):
+        return self.pub_data.strftime("%b %e %Y")
+        # return self.pub_data
---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+import blog.views
+
+urlpatterns = [
+    path('',blog.views.allblogs, name='allblogs'),
+    path('<int:blog_id>/', blog.views.detail, name='detail'),
+]
